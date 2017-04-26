0498de454c7652cd1136b47381f59ab69dd1314a
Eben <me@ebenroux.co.za>
2017-04-26 10:26:50 +0200

    - fixture passing

--- Shuttle.Esb.Module.Throttle.Tests/ThrottlePolicyFixture.cs
+++ Shuttle.Esb.Module.Throttle.Tests/ThrottlePolicyFixture.cs
@@ -7,11 +7,23 @@ namespace Shuttle.Esb.Module.Throttle.Tests
     public class ThrottlePolicyFixture
     {
         [Test]
-        public void Should_be_able_to_()
+        public void Should_be_able_to_determine_whether_to_abort()
         {
             var policy = new ThrottlePolicy(new ThrottleConfiguration());
 
             Assert.IsFalse(policy.ShouldAbort());
+
+            var aborted = false;
+            var timeout = DateTime.Now.AddSeconds(2);
+
+            policy = new ThrottlePolicy(new ThrottleConfiguration { CpuUsagePercentage = 1 });
+
+            while (!aborted && DateTime.Now < timeout)
+            {
+                aborted = policy.ShouldAbort();
+            }
+
+            Assert.IsTrue(aborted);
         }
     }
 }
