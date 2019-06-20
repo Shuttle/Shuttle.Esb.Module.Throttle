360004dd3cafa0423bce0f7e1f5ef8b490c7d24f
Eben <me@ebenroux.co.za>
2019-06-20 17:06:11 +0200

    - v11.0.0

--- Shuttle.Esb.Module.Throttle/Properties/AssemblyInfo.cs
+++ Shuttle.Esb.Module.Throttle/Properties/AssemblyInfo.cs
@@ -1,38 +1,14 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-#if NET46
-[assembly: AssemblyTitle(".NET Framework 4.6")]
-#endif
-
 #if NET461
 [assembly: AssemblyTitle(".NET Framework 4.6.1")]
 #endif
 
-#if NET462
-[assembly: AssemblyTitle(".NET Framework 4.6.2")]
-#endif
-
-#if NET47
-[assembly: AssemblyTitle(".NET Framework 4.7")]
-#endif
-
-#if NET471
-[assembly: AssemblyTitle(".NET Framework 4.7.1")]
-#endif
-
-#if NETCOREAPP2_0
-[assembly: AssemblyTitle(".NET Core 2.0")]
-#endif
-
-#if NETSTANDARD2_0
-[assembly: AssemblyTitle(".NET Standard 2.0")]
-#endif
-
-[assembly: AssemblyVersion("10.0.0.0")]
-[assembly: AssemblyCopyright("Copyright © Eben Roux 2018")]
+[assembly: AssemblyVersion("11.0.0.0")]
+[assembly: AssemblyCopyright("Copyright © Eben Roux 2019")]
 [assembly: AssemblyProduct("Shuttle.Esb.Module.Throttle")]
 [assembly: AssemblyCompany("Shuttle")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyInformationalVersion("10.0.0")]
+[assembly: AssemblyInformationalVersion("11.0.0")]
 [assembly: ComVisible(false)]
